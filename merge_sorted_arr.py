88d6919803228c9feae4e3151e3f289141227f98
Jon Chen <jonathan.chen.6799@gmail.com>
2021-09-01 16:59:12 -0700

    deletenode

--- merge_sorted_arr.py
+++ merge_sorted_arr.py
@@ -1,8 +1,8 @@
 while m > 0 and n > 0:
-            if nums1[m - 1] > nums2[n - 1]:
-                nums1[m + n - 1] = nums1[m - 1]
-                m -= 1
-            else:
-                nums1[m + n - 1] = nums2[n - 1]
-                n -= 1
-        nums1[:n] = nums2[:n]
+  if nums1[m - 1] > nums2[n - 1]:
+      nums1[m + n - 1] = nums1[m - 1]
+      m -= 1
+  else:
+      nums1[m + n - 1] = nums2[n - 1]
+      n -= 1
+  nums1[:n] = nums2[:n]
