1183e515009eecf8ab209cfe54d849055f3fa2ff
Jon Chen <jonathan.chen.6799@gmail.com>
2021-09-01 17:01:09 -0700

    delete node

--- delete_node.py
+++ delete_node.py
@@ -1,3 +1,9 @@
 def deleteNode(self, node):
   node.val = node.next.val
   node.next = node.next.next
+
+# Time complexity: O(1)
+# Space complexity: O(1)
+
+# Recognizing that deleting a node does not necessarily mean removing it from memory
+# Copying the next node and changing the reference essentially "deletes" it
