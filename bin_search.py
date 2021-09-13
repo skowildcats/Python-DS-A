09634885d04e45870e46730cadcc62e1048ac552
Jon Chen <jonathan.chen.6799@gmail.com>
2021-09-13 12:48:58 -0700

    fixed bin search

--- bin_search.py
+++ bin_search.py
@@ -1,19 +1,23 @@
 nums = [2, 5, 10, 11, 23, 25, 50, 130]
 
-def bin_search(nums, target):
-  start = 0
-  end = len(nums) - 1
 
-  while start <= end:
-    mid = (start + end) // 2
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
 
-    if nums[mid] == target:
-      return mid
-    elif nums[mid] < target:
-      start = mid + 1
-    else:
-      end = mid - 1
-  return start
+        while start <= end:
+            mid = (start + end) // 2
+
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+        return -1
+
+
 
 print(bin_search(nums, 3))
   
